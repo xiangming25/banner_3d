aa549f6ac6df0599e851ca8229a15ad18c840b70
xiangming <xiangming25@163.com>
2016-12-17 12:17:50 +0800

    finish project

---
+++ scripts/banner.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+	banner();
+}
+
+
+function banner(){
+	var bg = $('.bg'),
+		banner = $('.banner'),
+		img = $('.img'),
+		imgLeft = img.offset().left,
+		imgTop = img.offset().top,
+		imgWidth = img.width(),
+		imgHeight = img.height(),
+		oldPageX = imgLeft + imgWidth/2,
+		oldPageY = imgTop + imgHeight/2,
+		bathRandom = Math.random()*10;
+	bg.on('mousemove',function(e){
+		var _eventMove = window.event || e,
+			newPageX = _eventMove.pageX,
+			newPageY = _eventMove.pageY,
+			reduceX = -bathRandom+(newPageX - oldPageX)/50,
+			reduceY = -10+(newPageY - oldPageY)/50;
+		console.log(reduceX+"    "+reduceY);
+		banner.css('transform','rotateX('+reduceX+'deg) rotateY('+reduceY+'deg)');
+	});
+}
